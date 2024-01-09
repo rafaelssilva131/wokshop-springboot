2a897950ae469b9430f19dd1c8fe84e50a6e4ee6
Rafael Souza <rafael.ssilva131@gmail.com>
2024-01-08 21:30:09 -0300

    craido o findById()

---
+++ src/main/java/com/cursoudemy/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.cursoudemy.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursoudemy.course.entites.User;
+import com.cursoudemy.course.repositoreis.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.get();
+	}
+	
+}
