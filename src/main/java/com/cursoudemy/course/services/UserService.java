3fb3bc01c0edcd108f36985176814b9a84dbfdd4
Rafael Souza <rafael.ssilva131@gmail.com>
2024-01-09 20:23:33 -0300

    Exception handling - update

--- src/main/java/com/cursoudemy/course/services/UserService.java
+++ src/main/java/com/cursoudemy/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.cursoudemy.course.repositoreis.UserRepository;
 import com.cursoudemy.course.resources.exceptions.DatabaseException;
 import com.cursoudemy.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -43,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = userRepository.getReferenceById(id);
 		updateData(entity, obj);
 		return userRepository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
