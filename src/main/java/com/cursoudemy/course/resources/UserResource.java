eee780274b6913ccbae12aa3f2fe22dea5fd23ca
Rafael Souza <rafael.ssilva131@gmail.com>
2024-01-09 18:47:15 -0300

    User insert

--- src/main/java/com/cursoudemy/course/resources/UserResource.java
+++ src/main/java/com/cursoudemy/course/resources/UserResource.java
@@ -1,14 +1,18 @@
 package com.cursoudemy.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cursoudemy.course.entites.User;
 import com.cursoudemy.course.services.UserService;
@@ -31,5 +35,13 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = userService.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+				return ResponseEntity.created(uri).body(obj);
 	}
 }
