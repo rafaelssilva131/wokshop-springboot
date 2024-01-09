144a41a2965a4539b41e028e81aec6c8db3bd13d
Rafael Souza <rafael.ssilva131@gmail.com>
2024-01-09 15:03:04 -0300

    cirado Category

--- src/main/java/com/cursoudemy/course/config/TestConfig.java
+++ src/main/java/com/cursoudemy/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.cursoudemy.course.entites.Category;
 import com.cursoudemy.course.entites.Order;
 import com.cursoudemy.course.entites.User;
 import com.cursoudemy.course.entites.enums.OrderStatus;
+import com.cursoudemy.course.repositoreis.CategoryRepository;
 import com.cursoudemy.course.repositoreis.OrderRepository;
 import com.cursoudemy.course.repositoreis.UserRepository;
 
@@ -23,10 +25,18 @@ public class TestConfig implements CommandLineRunner {
 	private UserRepository userRepository;
 	@Autowired
 	private OrderRepository orderRepository;
+	@Autowired	
+	private CategoryRepository categoryRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
-
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
