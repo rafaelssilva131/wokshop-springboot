60859e9c9e580ccaa4cf50ce6fd9cf086e4aec96
Rafael Souza <rafael.ssilva131@gmail.com>
2024-01-09 16:47:57 -0300

    Payment, one-to-one association

--- src/main/java/com/cursoudemy/course/config/TestConfig.java
+++ src/main/java/com/cursoudemy/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.cursoudemy.course.entites.Category;
 import com.cursoudemy.course.entites.Order;
 import com.cursoudemy.course.entites.OrderItem;
+import com.cursoudemy.course.entites.Payment;
 import com.cursoudemy.course.entites.Product;
 import com.cursoudemy.course.entites.User;
 import com.cursoudemy.course.entites.enums.OrderStatus;
@@ -80,5 +81,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.saveAll(Arrays.asList(o1));
 	}
 }
